fb0fc1c404e11e82e641736ae9adef013fb6d60b
krishnasai2006 <krishnasai11229@gmail.com>
2026-03-14 19:27:36 +0530

    Add files via upload

---
+++ FSAD_EXP-4/README.txt
@@ -0,0 +1,5 @@
+FSAD Skill-4 Spring Dependency Injection
+Name: Krishnasai
+Roll: 2400032393
+
+Run MainApp.java to see constructor injection using Spring XML.
---
+++ FSAD_EXP-4/src/main/java/com/spring/di/MainApp.java
@@ -0,0 +1,16 @@
+package com.spring.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        Student s = (Student) context.getBean("studentBean");
+        s.display();
+    }
+}
